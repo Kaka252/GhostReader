3bba01a9c3bae84671f4902d53621d7d78e1fb8b
kaka252 <kaka252@sina.com>
2017-01-07 21:17:07 +0800

    添加epub的书籍以及相应类库

--- app/src/main/java/com/zhouyou/readerghost/MainActivity.java
+++ app/src/main/java/com/zhouyou/readerghost/MainActivity.java
@@ -1,13 +1,28 @@
 package com.zhouyou.readerghost;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.iv_read_entry).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_read_entry:
+                Intent intent = new Intent(this, ReaderActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/zhouyou/readerghost/ReaderActivity.java
@@ -0,0 +1,73 @@
+package com.zhouyou.readerghost;
+
+import android.app.Activity;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.TOCReference;
+import nl.siegmann.epublib.epub.EpubReader;
+
+/**
+ * Created by zhouyou on 17/1/7.
+ */
+
+public class ReaderActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reader);
+        findViewById(R.id.iv_close).setOnClickListener(this);
+        findViewById(R.id.iv_menu).setOnClickListener(this);
+        findViewById(R.id.iv_marker).setOnClickListener(this);
+
+//        AssetManager assetManager = getAssets();
+//        try {
+//            InputStream epubInputStream = assetManager.open("ghost_lantern.epub");
+//            Book book = (new EpubReader()).readEpub(epubInputStream);
+//            Log.i("epublib", "author(s): " + book.getMetadata().getAuthors());
+//            Log.i("epublib", "title: " + book.getTitle());
+//            logTableOfContents(book.getTableOfContents().getTocReferences(), 0);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    private void logTableOfContents(List<TOCReference> tocReferences, int depth) {
+        if (tocReferences == null) return;
+        for (TOCReference tocReference : tocReferences) {
+            StringBuilder tocString = new StringBuilder();
+            for (int i = 0; i < depth; i++) {
+                tocString.append("\t");
+            }
+            tocString.append(tocReference.getTitle());
+            Log.i("epublib", tocString.toString());
+
+            logTableOfContents(tocReference.getChildren(), depth + 1);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_close:
+                finish();
+                break;
+            case R.id.iv_menu:
+                break;
+            case R.id.iv_marker:
+                break;
+            default:
+                break;
+        }
+    }
+}
