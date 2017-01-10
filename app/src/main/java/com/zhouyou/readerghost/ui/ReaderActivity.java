43bdf1a8817334891790e9cb3a2bf8d28a7d3859
zhouyou <kaka252@sina.com>
2017-01-10 18:16:03 +0800

    修改工程结构

--- app/src/main/java/com/zhouyou/readerghost/ui/ReaderActivity.java
+++ app/src/main/java/com/zhouyou/readerghost/ui/ReaderActivity.java
@@ -26,8 +26,6 @@ import nl.siegmann.epublib.epub.EpubReader;
  */
 
 public class ReaderActivity extends Activity implements View.OnClickListener {
-
-    private CurlView curlView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,56 +33,8 @@ public class ReaderActivity extends Activity implements View.OnClickListener {
         findViewById(R.id.iv_close).setOnClickListener(this);
         findViewById(R.id.iv_menu).setOnClickListener(this);
         findViewById(R.id.iv_marker).setOnClickListener(this);
-
-        int index = 0;
-        if (getLastNonConfigurationInstance() != null) {
-            index = (Integer) getLastNonConfigurationInstance();
-        }
-        curlView = (CurlView) findViewById(R.id.curl_view);
-        curlView.setPageProvider(new PageProvider(this));
-        curlView.setSizeChangedObserver(new SizeChangedObserver(curlView));
-        curlView.setCurrentIndex(index);
-        curlView.setBackgroundColor(0xFF202830);
         Book book = App.get().getBook();
-
-//        Log.i("epublib", "author(s): " + book.getMetadata().getAuthors());
-//        Log.i("epublib", "title: " + book.getTitle());
-//        logTableOfContents(book.getTableOfContents().getTocReferences(), 0);
-
     }
-
-//    private void logTableOfContents(List<TOCReference> tocReferences, int depth) {
-//        if (tocReferences == null) return;
-//        for (TOCReference tocReference : tocReferences) {
-//            StringBuilder tocString = new StringBuilder();
-//            for (int i = 0; i < depth; i++) {
-//                tocString.append("\t");
-//            }
-//            tocString.append(tocReference.getTitle());
-//            Log.i("epublib", tocString.toString());
-//
-//            logTableOfContents(tocReference.getChildren(), depth + 1);
-//        }
-//    }
-
-
-    @Override
-    public Object onRetainNonConfigurationInstance() {
-        return curlView.getCurrentIndex();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        curlView.onPause();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        curlView.onResume();
-    }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
