2432a6e1ffa3d268bea64516912827628e6acb68
zhouyou <kaka252@sina.com>
2017-01-12 14:53:54 +0800

    添加电子书详情页面的依赖

--- app/src/main/java/com/zhouyou/readerghost/ui/MainActivity.java
+++ app/src/main/java/com/zhouyou/readerghost/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.folioreader.activity.FolioActivity;
 import com.zhouyou.readerghost.R;
 
 public class MainActivity extends Activity implements View.OnClickListener {
@@ -20,8 +21,12 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.iv_read_entry:
-                Intent intent = new Intent(this, ReaderActivity.class);
+                Intent intent = new Intent(this, FolioActivity.class);
+                intent.putExtra(FolioActivity.INTENT_EPUB_SOURCE_TYPE, FolioActivity.EpubSourceType.ASSESTS);
+                intent.putExtra(FolioActivity.INTENT_EPUB_SOURCE_PATH, "ghost_lantern.epub");
                 startActivity(intent);
+//                Intent intent = new Intent(this, ReaderActivity.class);
+//                startActivity(intent);
                 break;
             default:
                 break;
